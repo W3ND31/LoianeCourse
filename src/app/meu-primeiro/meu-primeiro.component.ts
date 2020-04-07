3d495547f6174238c2721c366f3825b9fac44a41
Wendel Oliveira <wendel.oliveira@solutis.com.br>
2020-04-07 10:32:34 -0300

    primeiro commit

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "meu-primeiro-component",
+  template: ` <p>Meu primeiro compnent com Angular 6</p> `,
+})
+class MeuPrimeiroComponent {}
+
+export default MeuPrimeiroComponent;
